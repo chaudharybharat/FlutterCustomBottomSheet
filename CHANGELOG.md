f9dce51f5a81af0146fbf0fe01a20dd3c7f8d88b
bcchaudhary <bharartchaudhary20893@gmail.com>
2020-11-02 10:38:30 +0530

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+## [0.2.0] - Sep 18, 2019
+
+* Added [CustomScaffold](https://github.com/ayham95/Custom-navigator/tree/master/custom_scaffold_example)
+* updated Readme.md
+
+
+## [0.1.1] - Sep 17, 2019
+
+Updated readme and added an example
+
+
+## [0.1.0] - Sep 17, 2019
+
+Initial Version of the library.
+
+* Includes the ability to use a new navigator anywhere 
+in the widget tree without the being dependent on App Navigator
---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/harsh/Documents/software/flutter_linux_1.20.3-stable/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/harsh/Downloads/Custom-navigator-master/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/harsh/Documents/software/flutter_linux_1.20.3-stable/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
